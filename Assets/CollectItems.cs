a814bd9ad3ae1a9c1e0b595db4cc4991c1458a4b
dragana16 <dragana.milovan@student.upt.ro>
2025-01-12 19:23:33 +0200

    Death message before respawn

--- Assets/CollectItems.cs
+++ Assets/CollectItems.cs
@@ -2,17 +2,26 @@ using UnityEngine;
 
 public class CollectItem : MonoBehaviour
 {
-    public int points = 10;   
-    public ScoreManager scoreManager;
+    public int points = 10;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
+        {
+            ScoreManager scoreManager = Object.FindFirstObjectByType<ScoreManager>();
+            ItemSpawner spawner = Object.FindFirstObjectByType<ItemSpawner>();  
+
             if (scoreManager != null)
             {
                 scoreManager.AddScore(points);
-
-                Destroy(gameObject);
             }
+
+            if (spawner != null)
+            {
+                spawner.ItemCollected(); 
+            }
+
+            Destroy(gameObject);
+        }
     }
 }
